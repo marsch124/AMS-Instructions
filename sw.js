607513e9151667519fbc83a388cb1b5fc89c82e0
MartinS <131461628+marsch124@users.noreply.github.com>
2026-08-12 19:08:24 +0200

    strul

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'ams-instructions-v4';
-const APP_VERSION = '4.0';
+const CACHE_NAME = 'ams-instructions-v5';
+const APP_VERSION = '5.0';
 
 const urlsToCache = [
     '/AMS-Instructions/',
